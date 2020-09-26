2b682570aae2442557f21a36a325df03705233ec
Vasily Vasinov <des.elyon@gmail.com>
2020-09-25 18:01:56 -0600

    Update app:create and app:update to zip up current app directory with git archive and send it over to the API as a file.

--- commands/apps/create.sh
+++ commands/apps/create.sh
@@ -1,4 +1,14 @@
+archive_name="app.zip"
+
+rm_archive() {
+  rm "$archive_name"
+}
+
+git archive -o "$archive_name" --format zip HEAD
+
+trap "rm_archive" EXIT
+
 curl -X POST \
   -H "Authorization: bearer ${OCEAN_ACCESS_TOKEN}" \
-  --data-binary @app.yaml \
+  -F "file=@$archive_name;type=application/zip" \
   "$OCEAN_URL"/api/v1/apps
--- commands/apps/update.sh
+++ commands/apps/update.sh
@@ -1,10 +1,20 @@
+archive_name="app.zip"
+
+rm_archive() {
+  rm "$archive_name"
+}
+
 if [ -z "$1" ]
 then
   echo "Provide an app ID."
   exit 1
 else
+  git archive -o "$archive_name" --format zip HEAD
+
+  trap "rm_archive" EXIT
+
   curl -X PUT \
     -H "Authorization: bearer ${OCEAN_ACCESS_TOKEN}" \
-    --data-binary @app.yaml \
+    -F "file=@$archive_name;type=application/zip" \
     "$OCEAN_URL"/api/v1/apps/"$1"
 fi
